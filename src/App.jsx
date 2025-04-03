f32dd6bcad5c56d9a5c3043c3bf6d953c5accae3
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-04-03 19:35:31 +0100

    add routes for pages (not connected to api yet)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Alerts from "./pages/Alerts.jsx";
 import Profile from "./pages/Profile.jsx";
 import Layout from "./components/Layout.jsx";
 
 export default function App() {
+  const pages = [
+    {
+      "page_id": "string",
+      "title": "string",
+      "components": [
+        {
+          "name": "string",
+          "component_id": "string"
+        }
+      ]
+    },
+    {
+      "page_id": "strin2",
+      "title": "string2",
+      "components": [
+        {
+          "name": "string2",
+          "component_id": "string2"
+        }
+      ]
+    }
+  ];
 
   return (
     <Router>
       <Routes>
         <Route path="/app/*" element={<Layout />}>
           <Route path="home" element={<Home />} />
-          <Route path="alerts" element={<Alerts />} />
           <Route path="profile" element={<Profile />} />
+          {pages.map((page) => (
+            <Route
+              key={page.page_id}
+              path={page.page_id}
+              element={
+                <div className="w-full min-h-svh p-8">
+                  <h1>{page.title}</h1>
+                  {page.components.map((component) => (
+                    <div key={component.component_id}>
+                      <h2>{component.name}</h2>
+                    </div>
+                  ))}
+                </div>
+              }
+            />
+          ))}
         </Route>
       </Routes>
     </Router>
